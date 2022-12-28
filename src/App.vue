43cccf106abca71cd218448d35a6847cbf0f69fd
ngmthaq <nguyenmanhthang2000.fb@gmail.com>
2022-12-28 22:46:14 +0700

    Init loading

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,26 @@
   <v-app>
     <v-main>
       <router-view />
+      <loading :open="$store.state.common.isLoading"></loading>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Loading from "@/components/common/Loading.vue";
+
 export default {
   name: "App",
+
+  components: {
+    loading: Loading,
+  },
 
   data() {
     return {};
   },
 
-  created() {
-    console.log(this.$store.state.common.isLoading);
-  },
+  created() {},
 
   mounted() {},
 };
---
+++ src/components/common/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loading" v-show="open">
+    <v-progress-linear indeterminate></v-progress-linear>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loading",
+
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.loading {
+  z-index: $z-index-loading;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: rgba($color: #000000, $alpha: 0.1);
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
